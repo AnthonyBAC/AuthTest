c54e94482f2e24bac411d03a931ffa2f8c97b24c
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-06-04 19:37:39 -0400

    eRole/Role model ok

---
+++ src/main/java/com/authTest/authTest/model/ERole.java
@@ -0,0 +1,6 @@
+package com.authTest.authTest.model;
+
+public enum ERole {
+    ROLE_ADMIN,
+    ROLE_USER
+}
---
+++ src/main/java/com/authTest/authTest/model/Role.java
@@ -0,0 +1,33 @@
+package com.authTest.authTest.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+}
